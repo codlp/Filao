4a3e852b65111b6127783e0533ee71e4f6b0316d
Grace Mia <grace.mehrabe@gmail.com>
2019-02-25 17:30:59 +0100

    added models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :project
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :documents, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_161800) do
+ActiveRecord::Schema.define(version: 2019_02_25_162556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "documents", force: :cascade do |t|
+    t.bigint "task_id"
+    t.string "name"
+    t.date "upload_date"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_documents_on_task_id"
+    t.index ["user_id"], name: "index_documents_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "task_id"
+    t.text "content"
+    t.date "upload_date"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_messages_on_task_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -57,6 +79,10 @@ ActiveRecord::Schema.define(version: 2019_02_25_161800) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "documents", "tasks"
+  add_foreign_key "documents", "users"
+  add_foreign_key "messages", "tasks"
+  add_foreign_key "messages", "users"
   add_foreign_key "tasks", "projects"
   add_foreign_key "tasks", "users"
 end
