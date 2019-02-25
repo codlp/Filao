35fdb58bb4dbcd1215ad2792a4fd9aaabe7b158f
Grace Mia <grace.mehrabe@gmail.com>
2019-02-25 18:12:17 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :projects do
+    resources :tasks
+  end
+  resources :documents, only: [:index, :new, :create, :destroy]
+  resources :messages, only: [:index, :new, :create, :update, :edit]
 end
