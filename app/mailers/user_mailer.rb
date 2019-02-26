b35562fb2df4667cfcb987fb7abcdc35e0523f98
Elie Alloun <eliealloun21@gmail.com>
2019-02-26 15:31:21 +0100

    mail connect

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user # Instance variable => available in view
 
-    mail(to: @user.email, subject: 'Welcome to Le Wagon')
+    mail(to: @user.email, subject: 'Welcome to Filao !')
     # This will render a view in `app/views/user_mailer`!
   end
 end
