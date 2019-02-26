b72078ca64d8ed47355d144905d73a81172cefc5
Grace Mia <grace.mehrabe@gmail.com>
2019-02-26 11:52:28 +0100

    added profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
+  resources :users, only: [:show, :edit, :update]
   resources :projects do
     resources :tasks
   end
