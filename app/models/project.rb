386ee9bcb9bcd23efa26baad9a851e9525587092
Hamza Amor <hamza.amor93@gmail.com>
2019-02-27 17:29:18 +0100

    commit new and edit project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
   belongs_to :user, class_name: 'User'
   belongs_to :customer, class_name: 'User', required: false
   has_many :tasks, dependent: :destroy
+  validates :category, inclusion: ["Web development", "Software", "Mobile", "Security"]
 end
