c7d9d4965a3b6307b69fca98ffc8c93e840e2afa
Elie Alloun <eliealloun21@gmail.com>
2019-02-28 16:02:56 +0100

    some modif

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,16 @@ import { loadDynamicBannerText } from '../components/banner';
 import initFullCalendar from '../components/fullcalendar';
 import initDropzone from '../components/dropzone';
 import "../components/flatpickr"
+import { initSweetalert } from '../components/init_sweetalert';
+import { initSortable } from '../components/init_sortable';
+
+initSweetalert('#sweet-alert', {
+  title: "Profil Update",
+  text: "Great your profile is up-to-date",
+  icon: "success"
+}, (value) => {
+  console.log(value);
+});
 
 if (document.getElementById('banner-typed-text')) {
 }
@@ -10,8 +20,9 @@ if (document.getElementById('banner-typed-text')) {
 if (document.getElementById('calendar')) {
   initFullCalendar();
 }
-  loadDynamicBannerText();
 
-// loadDynamicBannerText();
+
+initSweetalert();
+loadDynamicBannerText();
 initDropzone();
 
