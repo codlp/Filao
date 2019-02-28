cb2a84d789c2fdf1fd2644712df1e4dbdd1420ae
Coralie Delpha <coralie.delpha@gmail.com>
2019-02-28 12:52:12 +0100

    dropzone is working

--- db/schema.rb
+++ db/schema.rb
@@ -71,8 +71,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_134754) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encryptePOST **`reviews_path`**
-d_password", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -81,10 +80,10 @@ d_password", default: "", null: false
     t.string "photo"
     t.string "first_name"
     t.string "last_name"
-    t.boolean "is_customer", default: false
-    t.string "company"
     t.string "google_token"
     t.string "google_refresh_token"
+    t.boolean "is_customer", default: false
+    t.string "company"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
