b477de61aa9b953b6a0d8ba33e111e4260f0468d
Coralie Delpha <coralie.delpha@gmail.com>
2019-02-28 11:15:02 +0100

    drag and drop set up

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,8 @@ ActiveRecord::Schema.define(version: 2019_02_27_134754) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encryptePOST **`reviews_path`**
+d_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
