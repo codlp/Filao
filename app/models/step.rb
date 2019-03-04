83846c084a181affabd388088f938415d3fc3d6d
Grace Mia <grace.mehrabe@gmail.com>
2019-03-04 16:19:43 +0100

    changed progress bar

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,4 +3,10 @@ class Step < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :messages, through: :tasks
   has_many :documents, through: :tasks
+
+  def progress
+    tasks = self.tasks
+    done_tasks = tasks.where(is_done: true)
+    progress = done_tasks.size.fdiv(tasks.size) * 100
+  end
 end
