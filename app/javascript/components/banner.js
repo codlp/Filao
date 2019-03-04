71f41fafbf46dabca3a551154a429c601de785ff
Grace Mia <grace.mehrabe@gmail.com>
2019-03-04 13:15:30 +0100

    toggle messages

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["The perfect dashboard for Freelancers"],
-    typeSpeed: 50,
-    loop: true
-  });
+  const bannerTypedText = document.getElementById('banner-typed-text');
+  if (bannerTypedText) {
+    new Typed('#banner-typed-text', {
+      strings: ["The perfect dashboard for Freelancers"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 };
 
 export { loadDynamicBannerText };
